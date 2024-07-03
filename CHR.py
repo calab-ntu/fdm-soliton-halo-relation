4f5b3a915f28783424a65123ac861c2696b258e8
vivi235711 <vivi235711@gmail.com>
2024-07-03 10:59:00 +0800

    dd argparse to read input and set default cosmology

--- CHR.py
+++ CHR.py
@@ -1,11 +1,12 @@
+import argparse
+import sys
+
 import colossus
 import numpy as np
-
 from colossus.cosmology import cosmology
 from colossus.halo import concentration
 from colossus.utils import constants
 from scipy.integrate import quad
-
 
 ### constant
 kpc2km               = constants.KPC/1e5 # km
@@ -13,17 +14,24 @@ hbar                 = constants.H/2/np.pi/constants.KPC**2/constants.MSUN  # kp
 eV_c2                = constants.EV/constants.C**2/constants.MSUN # Msun
 newton_G             = constants.G/kpc2km**2 # (kpc^3)/(s^2*Msun)
 
-def get_cosmology():
+cosmo = cosmology.setCosmology('planck18')
+H0                   = cosmology.getCurrent().H0/1000 # km/s/kpc
+h                    = H0*10 # 100km/s/Mpc
+omega_M0             = cosmology.getCurrent().Om0
+background_density_0 = omega_M0*3*(H0/kpc2km)**2/(8*np.pi*newton_G) # Msun/kpc^3
+
+def set_cosmology(cosmology_name):
     """
-    Sets and returns the current cosmology.
+    Sets the current cosmology.
     """
-    cosmo = cosmology.setCosmology('planck18')
+    global cosmo, H0, h, omega_M0, background_density_0
+    cosmo = cosmology.setCosmology(cosmology_name)
     H0                   = cosmology.getCurrent().H0/1000 # km/s/kpc
     h                    = H0*10 # 100km/s/Mpc
     omega_M0             = cosmology.getCurrent().Om0
     background_density_0 = omega_M0*3*(H0/kpc2km)**2/(8*np.pi*newton_G) # Msun/kpc^3
 
-    return cosmo, H0, h, omega_M0, background_density_0
+    # return cosmo, H0, h, omega_M0, background_density_0
 
 
 def FDM_supress_laroche(M, particle_mass):
@@ -232,18 +240,33 @@ def revised_theo_c_FDM_Mc(current_redshift, Mh, particle_mass):
 
 if __name__ == '__main__':
 
-    ### set cosmology
-    cosmos, H0, h, omega_M0, background_density_0 = get_cosmology()
-    ### input your halo mass, particle mass, and redshift
-    halo_mass = 1e12
-    particle_mass = 2e-23
-    current_redshift = 0
 
-    print(f"halo mass: {halo_mass:.2e}, redshift: {current_redshift}, particle mass: {particle_mass}")
+    ### load the command-line parameters to input your halo mass, particle mass, and redshift
+
+    parser = argparse.ArgumentParser( description='Predicting the core-halo mass relation in fuzzy dark matter (FDM)' )
+
+    parser.add_argument( '--halo_mass', action='store', required=False, type=float, dest='halo_mass',
+                        help='halo mass', default=1e12 )
+    parser.add_argument( '--redshift', action='store', required=False,  type=float, dest='redshift',
+                        help='redshift', default=0 )
+    parser.add_argument( '--particle_mass', action='store', required=False,  type=float, dest='particle_mass',
+                        help='paticle mass', default=2e-23 )
+
+    args=parser.parse_args()
+
+    halo_mass           = args.halo_mass
+    current_redshift    = args.redshift
+    particle_mass       = args.particle_mass
+    
+    ### set cosmology
+    #set_cosmology('planck18') # you can change to other cos,ology
+    print(cosmology.getCurrent().name)
+
+    print(f"halo mass: {halo_mass:.2e}, redshift: {current_redshift:.2e}, particle mass: {particle_mass:.2e}")
 
     ### Calculate the revised core mass
     revised_c_FDM_Mc = revised_theo_c_FDM_Mc(current_redshift, halo_mass, particle_mass)
-    print(f"Predicted core mass (this work) : {revised_c_FDM_Mc}")
+    print(f"Predicted core mass (this work) : {revised_c_FDM_Mc:.2e}")
 
     ### Calculate the Schive2014 core mass
     theo_Mc = theo_TH_Mc(current_redshift, halo_mass, particle_mass)
